f9d279c2bc84a2155866e4fa62dc98b1fd5182b7
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-02-24 23:23:47 -0600

    Added timer and trigger

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,87 @@
-var myVar = setInterval(myTimer, 1000);
+// Selects element by id, it is selecting the span area that the time will display
+var timeEl = document.getElementById("time");
 
-function myTimer() {
-  var d = new Date();
-  document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+// Selects element by id, it is selecting the start button
+var mainEl = document.getElementById("start-button");
+
+var noticeEl = document.getElementById("notice");
+
+var questionEl = document.getElementById("question");
+
+document.getElementById("questions-section").style.display = "none";
+
+
+
+var secondsLeft = 30;
+
+function setTime(event) {
+  // Sets interval in variable
+
+  var timerInterval = setInterval(function() {
+    document.getElementById("button-div").style.display = "none";
+    document.getElementById("questions-section").style.display = "inline";
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left";
+
+    questions();
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
 }
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+
+ noticeEl.textContent = "Your time is over!";
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+}
+
+
+function questions() {
+  const question = [ 
+    { 
+      question: "What is JavaScript?",
+      options { 
+          a:"It is a programming language", 
+          b:"It is a fancy coffee bean", 
+          c:"", 
+          d:"" 
+      },
+      answer: "A" 
+    },
+    { 
+      question: "What is ?", 
+      options { 
+        a:"", 
+        b:"", 
+        c:"", 
+        d:"" 
+      },
+      answer: "C" 
+    },
+
+    {
+      question: "What is Ja?", 
+      options{ 
+        a:"",
+        b:"", 
+        c:"", 
+        d:"" 
+      },
+      answer: "B" 
+    }
+
+   ]
+}
+
+
+mainEl.addEventListener('click', setTime);
+
